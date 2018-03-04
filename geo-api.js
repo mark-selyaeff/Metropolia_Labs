d2c7864e05d399b7cc36561cc01395ca1ae2202c
Mark Selyaev <mark.selyaeff@yandex.ru>
2018-03-04 18:45:29 +0200

    Resolved mistakes in handling news for supported countries

--- geo-api.js
+++ geo-api.js
@@ -20,7 +20,23 @@ function showNews(pos) {
 			let country_code = res.results[0].address_components[0].short_name.toLowerCase()
 			if (country_code in countries_arr) {
 				let url_news = `https://newsapi.org/v2/top-headlines?country=${country_code}&apiKey=46db7fdbc25e4be89dee6b0f4cbef90b`
-				console.log('country code in arr ' + country_code);
+				news_req = new Request(url_news);
+				fetch(news_req)
+					.then(news_json => news_json.json())
+					.then(news => {
+						let news_arr = news.articles;
+						for (let i = 0; i < 5; i++) {
+							let node = document.createElement('LI');                 // Create a <li> node
+							// let textnode = document.createTextNode('Water');         // Create a text node
+							// node.appendChild(textnode);                              // Append the text to <li>
+							let link = document.createElement('A');
+							let textnode = document.createTextNode(news_arr[i].title);
+							link.appendChild(textnode);
+							link.href = news_arr[i].url;
+							node.appendChild(link);
+							document.getElementById('news').appendChild(node);
+						}
+					});
 			} else {
 				let url_news_req = new Request(`https://newsapi.org/v2/top-headlines?country=se&apiKey=46db7fdbc25e4be89dee6b0f4cbef90b`);
 				console.log('country code not in arr ' + country_code);
@@ -30,27 +46,17 @@ function showNews(pos) {
 					.then(news => {
 						let news_arr = news.articles;
 						for (let i = 0; i < 5; i++) {
-							let node = document.createElement("LI");                 // Create a <li> node
-							// let textnode = document.createTextNode("Water");         // Create a text node
+							let node = document.createElement('LI');                 // Create a <li> node
+							// let textnode = document.createTextNode('Water');         // Create a text node
 							// node.appendChild(textnode);                              // Append the text to <li>
-							let link = document.createElement("A");
+							let link = document.createElement('A');
 							let textnode = document.createTextNode(news_arr[i].title);
 							link.appendChild(textnode);
 							link.href = news_arr[i].url;
 							node.appendChild(link);
-							document.getElementById("news").appendChild(node);
+							document.getElementById('news').appendChild(node);
 						}
 					});
 			}
 		});
-	// currentCountry.innerHTML = country.long_name ? country.long_name : country.short_name;
 }
-
-// function getCountry(crd) {
-// 	let url = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${crd.latitude},${crd.longitude}&result_type=country&key=AIzaSyD8UePQnlJgy5tTJdYsnwVUtKE47ixtPEs`;
-// 	console.log(url);
-// 	const req = new Request(url);
-// 	return fetch(req)
-// 		.then(response => response.json())
-// 		.then(res => {return res.results[0].address_components[0]});
-// }
